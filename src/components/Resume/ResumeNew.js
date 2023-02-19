cb376851f71cdf33a6315a9699bedc20e9f45831
Rivan Nur Ihsan <rivannurihsan@Rivans-MacBook-Pro.local>
2023-02-19 18:47:26 +0700

    Portofolio V1

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -9,7 +9,7 @@ import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
 const resumeLink =
-  "https://drive.google.com/file/d/1kNDK_C8oS-tOVMs0Tg_8FDQ8_MI_-9sC/view?usp=sharing";
+  "https://raw.githubusercontent.com/rivannurihsan/portfolio/mastersrc/Assets/portofolio.pdf";
 
 function ResumeNew() {
   const [width, setWidth] = useState(1200);
